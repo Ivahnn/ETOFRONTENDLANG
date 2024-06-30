af832446830393b4c6774884e944bcf9bea26771
Ivahnn Garcia <migarcia@tip.edu.ph>
2024-06-30 21:28:01 +0800

    first commit

---
+++ react-johnpharmassist/src/components/pages/Aboutus.js
@@ -0,0 +1,15 @@
+import React from "react";
+import '../../App.css'; // Updated import path
+import About from '../About';
+import Footer from "../Footer";
+
+
+export default function Aboutus() {
+  return (
+    <>
+    {/* <h1 className="aboutus">ABOUT US</h1> */}
+      <About />
+      <Footer/>
+    </>
+  );
+}
---
+++ react-johnpharmassist/src/components/pages/Contactus.js
@@ -0,0 +1,14 @@
+import React from "react";
+import '../../App.css'; // Updated import path
+import Contact from '../Contact';
+import Footer from "../Footer";
+
+
+export default function Contactus() {
+  return (
+    <>
+      <Contact />
+      <Footer/>
+    </>
+  );
+}
